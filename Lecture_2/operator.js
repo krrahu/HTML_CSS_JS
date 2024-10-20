bf84219cb6c113a52a8a0f96071da26a3030814c
krrahu <rahul.baliyase91@gmail.com>
2024-10-20 17:27:16 +0530

    test

---
+++ Lecture_2/operator.js
@@ -0,0 +1,8 @@
+let a = prompt("enter the number");
+if (a % 2 === 0) {
+    console.log("even number" + a)
+} else {
+    console.log("odd number" + a)
+}
+
+console.log(alert(" prgm end")); // one time pop-upd
